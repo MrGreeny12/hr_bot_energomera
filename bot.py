e4a35161f130f93fdd2181e9cc527af4986e25c3
viktor <ivakhnenkovd@yandex.ru>
2021-02-28 19:57:48 +0300

    create employee block (schema)

--- bot.py
+++ bot.py
@@ -1,154 +1,145 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import telebot
 from config import TOKEN
 import keyboards as kb
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+token = TOKEN
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def process_start_command(message):
+    bot.send_message(message.chat.id,
+                     'Привет! Я чат-бот HR-службы Энергомера. Пока я работаю в тестовом режиме, '
+                     'но ты можешь для начала, рассказать, кто ты!',
+                     reply_markup=kb.keyboardWelcome)
 
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    '''
-    стартовое сообщение при запуске бота
-    '''
-    await message.reply('Привет! Я чат-бот HR-службы Энергомера. Пока я работаю в тестовом режиме, '
-                        'но ты можешь для начала, рассказать, кто ты!', reply_markup=kb.keyboardWelcome)
+@bot.message_handler(commands=['help'])
+def process_help_command(message):
+    bot.send_message(message.chat.id, 'Здесь по идее будет инфа о полезностях бота')
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    '''
-    информация о функциях бота. запускается командой /help
-    '''
-    await message.reply('Запусти меня - напиши /start')
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == 'kb.keyboardWelcome':
+        bot.edit_message_text('Привет! Я чат-бот HR-службы Энергомера. Пока я работаю в тестовом режиме, '
+                              'но ты можешь для начала, рассказать, кто ты!',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardWelcome)
+    elif call.data == 'employee':
+        bot.edit_message_text('Это раздел для сотрудников. Выбирай, не хочу!',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardEmployee)
+    elif call.data == 'candidate':
+        bot.edit_message_text('Это раздел для соискателей. Все самые распространнёные '
+                              'ответы на вопросы Вы можете найти в разделе FAQ. Ссылки на '
+                              'наши социальные сети Вы можете найти в соответствующем разделе',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardCandidate)
+    elif call.data == 'faq':
+        bot.edit_message_text('Выберите интересующий Вас вопрос:',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardFAQ)
+    elif call.data == 'socialNetworks':
+        bot.edit_message_text('Список наших сообществ в социальных сетях:',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardSocialNetworks)
+    elif call.data == 'steps':
+        bot.edit_message_text('Здесь будет информация об этапах подбора персонала',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'testResultCandidate':
+        bot.edit_message_text('Здесь будет информация о том, как узнать результаты тестирования',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'feedback':
+        bot.edit_message_text('Здесь ты можешь оставить своё никому не нужное мнение о нашей классной'
+                              ' службе УП. \n'
+                              '\n'
+                              'По этому адресу - test@energomera.ru',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'testTrouble':
+        bot.edit_message_text('Конечно ты всё сломал. Кто бы сомневался',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'docList':
+        bot.edit_message_text('Тут список всего, что нужно для приёма',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'studentsPractice':
+        bot.edit_message_text('Хочешь на практику? А вот хрен тебе',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'sendResume':
+        bot.edit_message_text('Отправить своё резюме можно на адрес - test@energomera.ru\n'
+                              '\n'
+                              'Тему сообщения нужно - такую то \n'
+                              'Мы обработаем Ваше резюме в течение N часов',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardPageCandidate)
+    elif call.data == 'motivation':
+        bot.edit_message_text('Тута раздел по мотивации',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardMotivation)
+    elif call.data == 'money':
+        bot.edit_message_text('Тута раздел о деньгах',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardMoney)
+    elif call.data == 'labourOrganization':
+        bot.edit_message_text('Тута раздел об организации труда',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardLabourOrganization)
+    elif call.data == 'dms':
+        bot.edit_message_text('Тута раздел о ДМС',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardDMS)
+    elif call.data == 'cnb':
+        bot.edit_message_text('Тута раздел о кадровом делопроизводстве',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardCnB)
+    elif call.data == 'differentiation':
+        bot.edit_message_text('Тута раздел о дифференциации персонала',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardDifferentiation)
+    elif call.data == 'mentoring':
+        bot.edit_message_text('Тута раздел о тех, кто учит Вас',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardMentoring)
+    elif call.data == 'study':
+        bot.edit_message_text('Тута раздел об обучении',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardStudy)
+    elif call.data == 'recruitment':
+        bot.edit_message_text('Тута раздел о подборе персонала',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardRecruitment)
+    elif call.data == 'covid19':
+        bot.edit_message_text('Тута раздел о короне и её носителях',
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=kb.keyboardCovid19)
 
 
-@dp.callback_query_handler(lambda c: c.data == 'candidate')
-async def process_callback_candidate(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Это раздел для соискателей. Все самые распространнёные '
-                                                        'ответы на вопросы Вы можете найти в разделе FAQ. Ссылки на '
-                                                        'наши социальные сети Вы можете найти в соответствующем '
-                                                        'разделе', reply_markup=kb.keyboardCandidate)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'faq')
-async def process_callback_candidate_faq(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Выберите интересующий Вас вопрос:',
-                           reply_markup=kb.keyboardFAQ)
-
-
-@dp.callback_query_handler(lambda c: c.data == 'steps')
-async def process_callback_candidate_faq_steps(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> этапы собеседования
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Здесь мы подробно распишем этапы рассмотрения, например:\n'
-                           '1. Офигенный этап.\n'
-                           '2. Ещё лучше.\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-
-
-@dp.callback_query_handler(lambda c: c.data == 'testResult')
-async def process_callback_candidate_faq_testResult(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> узнать результаты тестирования
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Здесь мы подробно расскажем о том, как узнать результаты тестирования:\n'
-                           '1. Позвонить туда.\n'
-                           '2. Спросить сюда.\n'
-                           '3. Пойти на хер например. Уж больно ты любопытный.\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-
-
-@dp.callback_query_handler(lambda c: c.data == 'feedback')
-async def process_callback_candidate_faq_feedback(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> дать обратную связь
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Давай, пёс. Расскажи, что тебе не понравилось!\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-    #нужно добавить форму обратной связи для записи. после этого вывести сообщение типа: заебись, принято!
-
-@dp.callback_query_handler(lambda c: c.data == 'testTrouble')
-async def process_callback_candidate_faq_testTrouble(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> проблемы с прохождением тестированияы
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Не получается пройти тестирование? Значит ты довольно тупой. Пшёл отсюда!\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-    # нужно добавить базовые варианты при некорректной работе тестирования и форму обратной связи для возможности
-    # прикреплять файл и сообщение
-
-
-@dp.callback_query_handler(lambda c: c.data == 'docList')
-async def process_callback_candidate_faq_docList(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> список документов для приёма
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Вот, что нужно для приёма на работу:\n'
-                           '1. Счастье.\n'
-                           '2. Здоровье.\n'
-                           '3. Удача.\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-    # нужно добавить список документов, которые требуются.
-
-
-@dp.callback_query_handler(lambda c: c.data == 'studentsPractice')
-async def process_callback_candidate_faq_studentsPractice(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> информация для прохождения практики студентов
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'Вот, что нужно для попадания на практику:\n'
-                           '1. Счастье.\n'
-                           '2. Здоровье.\n'
-                           '3. Удача.\n'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-    # нужно добавить список шагов, которые требуются.
-
-
-@dp.callback_query_handler(lambda c: c.data == 'sendResume')
-async def process_callback_candidate_faq_sendResume(callback_query: types.CallbackQuery):
-    '''
-    меню кандидата -> faq -> отправить своё резюме
-    '''
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id,
-                           'А здесь, пёс, ты должен приложить своё сраное резюме, чтобы оно дошло к нам. '
-                           'Прикладывай как подоржник к оторванной руке. '
-                           'Это настолько же эффективно. Тебе уже ничего не поможет.'
-                           '\n'
-                           'Для возвращения в начало, нажмите на -> /start')
-    # нужно добавить возможность прикладывать резюме и отправку его девчёнкам или в какой-нибудь чат.
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
+bot.polling()
--- keyboards.py
+++ keyboards.py
@@ -19,20 +19,141 @@ keyboardCandidate.add(keyFaq, keySocialNetworks).add(keyBackMainMenu)
 #клавиатура раздела FAQ
 keyboardFAQ = types.InlineKeyboardMarkup()
 keySteps = types.InlineKeyboardButton('Этапы подбора персонала', callback_data='steps')
-keyTestResult = types.InlineKeyboardButton('Узнать результаты тестирования', callback_data='testResult')
+keyTestResultCandidate = types.InlineKeyboardButton('Узнать результаты тестирования', callback_data='testResultCandidate')
 keyFeedback = types.InlineKeyboardButton('Хочу пожаловаться', callback_data='feedback')
 keyTestTrouble = types.InlineKeyboardButton('Не получается пройти тестирование', callback_data='testTrouble')
 keyDocList = types.InlineKeyboardButton('Документы для приёма на работу', callback_data='docList')
 keyStudentsPractice = types.InlineKeyboardButton('Как попасть на практику?', callback_data='studentsPractice')
 keySendResume = types.InlineKeyboardButton('Хочу отправить резюме', callback_data='sendResume')
 keyBackCandidate = types.InlineKeyboardButton(text='Назад', callback_data='candidate')
-keyboardFAQ.add(keySteps).add(keyTestResult).add(keyFeedback).add(keyTestTrouble).add(keyDocList)\
+keyboardFAQ.add(keySteps).add(keyTestResultCandidate).add(keyFeedback).add(keyTestTrouble).add(keyDocList)\
     .add(keyStudentsPractice).add(keySendResume).add(keyBackCandidate)
 
 
 #клавиатура раздела Социальные сети
 keyboardSocialNetworks = types.InlineKeyboardMarkup()
-keyVK = types.InlineKeyboardButton('ВКонтакте', url='wwww.com', callback_data='vk')
-keyFacebook = types.InlineKeyboardButton('Facebook', url='wwww.com', callback_data='facebook')
-keyInstagram = types.InlineKeyboardButton('Instagram', url='wwww.com', callback_data='instagram')
-keyboardSocialNetworks.add(keyVK).add(keyFacebook).add(keyInstagram).add(keyBackCandidate)
+keyVK = types.InlineKeyboardButton('ВКонтакте', url='http://vk.com/concern_energomera', callback_data='vk')
+keyFacebook = types.InlineKeyboardButton('Facebook', url='https://www.facebook.com/energomera.ru', callback_data='facebook')
+keyInstagram = types.InlineKeyboardButton('Instagram', url='https://instagram.com/energomera_careers?igshid=eynr8657nskp', callback_data='instagram')
+keyHeadHunter = types.InlineKeyboardButton('Head Hunter', url='https://stavropol.hh.ru/employer/24946', callback_data='headHunter')
+keyMainSite = types.InlineKeyboardButton('Официальный сайт Компании', url='http://www.energomera.com', callback_data='mainSite')
+keyboardSocialNetworks.add(keyVK).add(keyFacebook).add(keyInstagram).add(keyHeadHunter).add(keyMainSite).add(keyBackCandidate)
+
+
+#клавиатура для всех страниц с информацией
+keyboardPageCandidate = types.InlineKeyboardMarkup()
+keyMainMenu = types.InlineKeyboardButton('В начало', callback_data='kb.keyboardWelcome')
+keyBackFAQ = types.InlineKeyboardButton('Назад', callback_data='faq')
+keyboardPageCandidate.add(keyMainMenu).add(keyBackFAQ)
+
+
+#клавиатура для страницы сотрудника
+keyboardEmployee = types.InlineKeyboardMarkup()
+keyMotivation = types.InlineKeyboardButton('Мотивация персонала', callback_data='motivation')
+keyMoney = types.InlineKeyboardButton('Вопросы о деньгах', callback_data='money')
+keyLabourOrganization = types.InlineKeyboardButton('Организация труда', callback_data='labourOrganization')
+keyDMS = types.InlineKeyboardButton('Дополнительное медицинское страхование', callback_data='dms')#поменять название
+keyCnB = types.InlineKeyboardButton('Кадровое делопроизводство', callback_data='cnb')#поменять название
+keyDifferentiation = types.InlineKeyboardButton('Дифференциация персонала', callback_data='differentiation')
+keyMentoring = types.InlineKeyboardButton('Всё о наставниках', callback_data='mentoring')
+keyStudy = types.InlineKeyboardButton('Обучение персонала', callback_data='study')
+keyRecruitment = types.InlineKeyboardButton('Подбор персонала', callback_data='recruitment')
+keyCOVID19 = types.InlineKeyboardButton('Короновирус (COVID-19)', callback_data='covid19')
+keyBackMainMenu = types.InlineKeyboardButton(text='Назад', callback_data='kb.keyboardWelcome')
+keyboardEmployee.add(keyMotivation).add(keyMoney).add(keyLabourOrganization).add(keyDMS)\
+    .add(keyCnB).add(keyDifferentiation).add(keyMentoring).add(keyStudy)\
+    .add(keyRecruitment).add(keyCOVID19).add(keyBackMainMenu)
+
+
+#клавиатура раздела мотивация персонала
+keyboardMotivation = types.InlineKeyboardMarkup()
+keyActivity = types.InlineKeyboardButton('Хочу устроить концерт или хотя бы чаепитие!', callback_data='activity')
+keySpecialOffers = types.InlineKeyboardButton('Корпоративные скидки, акции и предложения', callback_data='specialOffers')
+keyBackEmployee = types.InlineKeyboardButton(text='Назад', callback_data='employee')
+keyboardMotivation.add(keyActivity).add(keySpecialOffers).add(keyBackEmployee)
+
+
+#клавиатура раздела о деньгах
+keyboardMoney = types.InlineKeyboardMarkup()
+keySalaryPayment = types.InlineKeyboardButton('Как начисляется заработная плата?', callback_data='salaryPayment')
+keyPrepayment = types.InlineKeyboardButton('Как рассчитывается аванс?', callback_data='prepayment')
+keySickLeave = types.InlineKeyboardButton('Как рассчитывать больничный?', callback_data='sickLeave')
+keyMaternityLeave = types.InlineKeyboardButton('Как рассчитываются декретные?', callback_data='maternityLeave')
+keyDoc2NDFL = types.InlineKeyboardButton('Как получить справку 2НДФЛ?', callback_data='doc2NDFL')
+keySalaryTrouble = types.InlineKeyboardButton('Не пришла заработная плата, что делать?', callback_data='salaryTrouble')
+keyboardMoney.add(keySalaryPayment).add(keyPrepayment).add(keySickLeave).add(keyMaternityLeave).add(keyDoc2NDFL)\
+    .add(keySalaryTrouble).add(keyBackEmployee)
+
+
+#клавиатура раздела об организации труда
+keyboardLabourOrganization = types.InlineKeyboardMarkup()
+keyPassLost = types.InlineKeyboardButton('Потерял пропуск, что делать?', callback_data='passLost')
+keyProhibitions = types.InlineKeyboardButton('Какие существуют запреты на предприятии?', callback_data='prohibitions')
+keyBenefits = types.InlineKeyboardButton('Что такое премии?', callback_data='benefits')
+keySchedule = types.InlineKeyboardButton('Узнать график работы', callback_data='schedule')
+keyRemote = types.InlineKeyboardButton('Как перейти на удалёнку?', callback_data='remote')
+keyboardLabourOrganization.add(keyPassLost).add(keyProhibitions).add(keyBenefits).add(keySchedule).add(keyRemote)\
+    .add(keyBackEmployee)
+
+
+#клавиатура раздела о ДМС (дополнительное медицинское страхование)
+keyboardDMS = types.InlineKeyboardMarkup()
+keyHowUseDMS = types.InlineKeyboardButton('Как воспользоваться ДМС?', callback_data='howUseDMS')
+keyCashOutDMS = types.InlineKeyboardButton('Можно ли обналичить ДМС?', callback_data='cashOutDMS')
+keyFamilyDMS = types.InlineKeyboardButton('Могу  ли я перевести ДМС на родственников?', callback_data='familyDMS')
+keyboardDMS.add(keyHowUseDMS).add(keyCashOutDMS).add(keyFamilyDMS).add(keyBackEmployee)
+
+
+#клавиатура раздела о кадровом делопроизводстве
+keyboardCnB = types.InlineKeyboardMarkup()
+keyLeaveWithoutPay = types.InlineKeyboardButton('Как взять отпуск без содержания?', callback_data='leaveWithoutPay')
+keyDayOff = types.InlineKeyboardButton('Что такое отгул?', callback_data='dayOff')
+keyDonor = types.InlineKeyboardButton('Что такое донорские?', callback_data='donor')
+keySickLeaveOut = types.InlineKeyboardButton('Что делать с больничным листом?', callback_data='sickLeaveOut')
+keyboardCnB.add(keyLeaveWithoutPay).add(keyDayOff).add(keyDonor).add(keySickLeaveOut).add(keyBackEmployee)
+
+
+#клавиатура раздела о дифференциации персонала
+keyboardDifferentiation = types.InlineKeyboardMarkup()
+keyWhenFeedback = types.InlineKeyboardButton('Когда ждать обратную связь?', callback_data='whenFeedack')
+keyExpertCommission = types.InlineKeyboardButton('Кто будет меня оценивать?', callback_data='expertCommission')
+keyMyDFGroup = types.InlineKeyboardButton('Как узнать в какой я группе?', callback_data='myDFGroup')
+keyboardDifferentiation.add(keyWhenFeedback).add(keyExpertCommission).add(keyMyDFGroup).add(keyBackEmployee)
+
+
+#клавиатура раздела о наставниках
+keyboardMentoring = types.InlineKeyboardMarkup()
+keyFailureMentor = types.InlineKeyboardButton('Наставник отсутствует, что делать?', callback_data='failureMentor')
+keyEditMentor = types.InlineKeyboardButton('Как можно поменять наставника?', callback_data='editMentor')
+keyboardMentoring.add(keyFailureMentor).add(keyEditMentor).add(keyBackEmployee)
+
+
+#клавиатура раздела об обучении
+keyboardStudy = types.InlineKeyboardMarkup()
+keyAttestation = types.InlineKeyboardButton('Аттестация персонала', callback_data='attestation')
+keyFailureStudy = types.InlineKeyboardButton('Преподаватель не провёл обучение', callback_data='failureStudy')
+keyTrainig = types.InlineKeyboardButton('Как мне повысить квалификацию?', callback_data='training')
+keyRemoteStudyPortal = types.InlineKeyboardButton('Можно ли попасть на портал обучения дистанционно?', callback_data='remoteStudyPortal')
+keyAbsenteeism = types.InlineKeyboardButton('Я не могу пройти обучение по плану - что делать?', callback_data='absenteeism')
+keyOrderQualificationCommission = types.InlineKeyboardButton('Как будет проходить квалификацонная комиссия?', callback_data='orderQualificationCommission')
+keyPreQualificationCommission = types.InlineKeyboardButton('Что готовить на квалификационную комиссию?', callback_data='preQualificationCommission')
+keyFailureQualificationCommission = types.InlineKeyboardButton('Что делать, если просрал всё перед комиссией?', callback_data='failureQualificationCommission')
+keyboardStudy.add(keyAttestation).add(keyFailureStudy).add(keyTrainig).add(keyRemoteStudyPortal)\
+    .add(keyAbsenteeism).add(keyOrderQualificationCommission).add(keyPreQualificationCommission)\
+    .add(keyFailureQualificationCommission).add(keyBackEmployee)
+
+
+#клавиатура раздела о подборе персонала
+keyboardRecruitment = types.InlineKeyboardMarkup()
+keyAboutReferal = types.InlineKeyboardButton('Подробно о реферальной программе', callback_data='aboutReferal')
+keyTestResultEmployee = types.InlineKeyboardButton('Как узнать результаты тестирования?', callback_data='testResultEmployee')
+keyboardRecruitment.add(keyAboutReferal).add(keyTestResultEmployee).add(keyBackEmployee)
+
+
+#клавиатура раздела о COVID-19
+keyboardCovid19 = types.InlineKeyboardMarkup()
+keyRecommendation = types.InlineKeyboardButton('Рекомендации от правительства Ставропольского края', callback_data='recommendation')
+keyActualDoc = types.InlineKeyboardButton('Актуальные документы', callback_data='actualDoc')
+keyRemoteStatement = types.InlineKeyboardButton('Заявление на удалёнку', callback_data='remoteStatement')
+keyboardCovid19.add(keyRecommendation).add(keyActualDoc).add(keyRemoteStatement)\
+    .add(keyBackEmployee)
